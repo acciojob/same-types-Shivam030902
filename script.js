734344ef2c5a1ac7bce761d01daee88f496fae29
Shivam030902 <153266080+Shivam030902@users.noreply.github.com>
2024-01-09 16:34:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function isSameType(value1, value2) {
     //your js code here
+	  if (isNaN(value1) && isNaN(value2)) {
+        return true;
+    }
       if((typeof value1) == (typeof value2))
       {
-        console.log("true");
+        return true;
       }
       else{
-        console.log("false");
+        return false;
       }
   }
   
